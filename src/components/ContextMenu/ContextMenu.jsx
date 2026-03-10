a7e699b6849eafb0e9b982289831740ac15c1414
paulgr98 <pawelwnuk98@gmail.com>
2026-03-10 18:12:46 +0100

    Add simple context menu

---
+++ src/components/ContextMenu/ContextMenu.jsx
@@ -0,0 +1,42 @@
+import './ContextMenu.css'
+
+const ContextMenu = ({
+    rightClickedItem,
+    positionX,
+    positionY,
+    isToggled,
+    buttons,
+    contextMenuRef
+}) => {
+    return (
+        <menu
+            style={{
+                top: positionY + 2 + 'px',
+                left: positionX + 2 + 'px'
+            }}
+            className={`context-menu ${isToggled ? 'active' : ''}`}
+            ref={contextMenuRef}
+        >
+            {buttons.map((button, index) => {
+                const handleClick = (e) => {
+                    e.stopPropagation()
+                    button.onClick(e, rightClickedItem)
+                }
+
+                return (
+                    <button
+                        onClick={handleClick}
+                        key={index}
+                        className='context-menu-button'
+                    >
+                        <span>{button.text}</span>
+                        <span className='icon'>{button.icon}</span>
+                    </button>
+                )
+            }
+            )}
+        </menu>
+    )
+}
+
+export default ContextMenu
